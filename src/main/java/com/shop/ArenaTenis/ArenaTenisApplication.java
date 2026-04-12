ffd21707729b23770211b728138265a52ce25890
ion anghel <ion35898@gmail.com>
2026-04-12 10:46:11 +0200

    Initial commit

---
+++ src/main/java/com/shop/ArenaTenis/ArenaTenisApplication.java
@@ -0,0 +1,13 @@
+package com.shop.ArenaTenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaTenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaTenisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/ArenaTenis/ArenaTenisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.ArenaTenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaTenisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
