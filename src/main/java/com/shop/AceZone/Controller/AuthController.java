9115d80e67e7402cd6c7cc07b6ab1bf4728d80ca
ionicka <ion35898@gmail.com>
2026-04-25 13:47:22 +0200

    AuthControler Add Exceptions false credentials'

--- src/main/java/com/shop/AceZone/Controller/AuthController.java
+++ src/main/java/com/shop/AceZone/Controller/AuthController.java
@@ -7,6 +7,7 @@ import com.shop.AceZone.Model.User;
 import com.shop.AceZone.Service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,17 +31,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest req) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword())
-        );
+    public ResponseEntity<?> login(@RequestBody LoginRequest req) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword())
+            );
 
-        User user = userService.findByUsername(req.getUsername())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+            User user = userService.findByUsername(req.getUsername())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
 
-        return jwtUtil.generateToken(req.getUsername(), user.getRole());
+            return ResponseEntity.ok(jwtUtil.generateToken(req.getUsername(), user.getRole()));
+
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(401).body("Username sau parola greșite");
+        }
     }
-
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest req) {
         try {
